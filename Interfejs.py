acbf4574fd4aea448485d317d3b5fa4cffcedef6
Marcin <marcinglodziak@gmail.com>
2020-11-09 10:58:41 +0100

    Konflikt

--- Interfejs.py
+++ Interfejs.py
@@ -12,7 +12,7 @@ entry=Entry(root, width=59, borderwidth=1,).grid(row=1, column=0, columnspan=4)
 label=Label(root, width=35, text="").grid(row=2, column=0, columnspan=3)
 
 button_next=Button(root, text="Następne równanie", fg="#ffed7a", bg="#a16d00", activeforeground="#431800", activebackground="#e49f33", padx="14", pady="10", font=myFont).grid(row=3, column=0, columnspan=2)
-button_clear=Button(root, text="Kasuj", fg="#875c00", bg="#e6d047", activeforeground="#ffed7a", activebackground="#bf1717", padx="27", pady="10", font=myFont).grid(row=4,column=0)
+button_clear=Button(root, text="Kasuj", fg="#875c00", bg="#e6d047", activeforeground="#ffed7a", activebackground="#000000", padx="27", pady="10", font=myFont).grid(row=4,column=0)
 button_x=Button(root, text="x", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=1)
 button_y=Button(root, text="y", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=2)
 button_z=Button(root, text="z", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=3)
