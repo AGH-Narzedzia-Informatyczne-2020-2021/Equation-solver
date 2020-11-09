5544e17bb1b90c5380020039fdb76b062d9264a8
Maja Swierk <xxtoyoukexx@gmail.com>
2020-11-09 20:02:43 +0100

    Added button

--- Interfejs.py
+++ Interfejs.py
@@ -6,16 +6,19 @@ root.title("Kalkulator")
 #root.geometry("500x200")
 myFont = font.Font(size=16)
 
+
 label=Label(root, width=35, text="").grid(row=0, column=0, columnspan=3)
 entry=Entry(root, width=59, borderwidth=1,).grid(row=1, column=0, columnspan=4)
 
 label=Label(root, width=35, text="").grid(row=2, column=0, columnspan=3)
 
+button_var=Button(root, text="zmienne", fg="#ffed7a", bg="#a16d00", activeforeground="#431800", activebackground="#e49f33", padx="14", pady="10", font=myFont).grid(row=3, column=2, columnspan=2)
 button_next=Button(root, text="Następne równanie", fg="#ffed7a", bg="#a16d00", activeforeground="#431800", activebackground="#e49f33", padx="14", pady="10", font=myFont).grid(row=3, column=0, columnspan=2)
 button_clear=Button(root, text="Kasuj", fg="#875c00", bg="#e6d047", activeforeground="#ffed7a", activebackground="#bf1717", padx="27", pady="10", font=myFont).grid(row=4,column=0)
 button_x=Button(root, text="x", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=1)
 button_y=Button(root, text="y", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=2)
 button_z=Button(root, text="z", fg="#875c00", bg="#e6d047", activeforeground="#431800", activebackground="#f7e158", padx="51", pady="10", font=myFont).grid(row=4,column=3)
+
 
 button1=Button(root, text="1", fg="#875c00", bg="#ffff9c", activeforeground="#875c00", activebackground="#ffffcf", padx="50", pady="20", font=myFont).grid(row=5,column=0)
 button2=Button(root, text="2", fg="#875c00", bg="#ffff9c", activeforeground="#875c00", activebackground="#ffffcf", padx="50", pady="20", font=myFont).grid(row=5,column=1)
